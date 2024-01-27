30b4d918b4e7d17ba936bf8c9bd57d6afc93f1a0
Anish-bhardwaj <anishbhardwaj1342005@gmail.com>
2024-01-27 20:48:12 +0530

    API Commit

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = 3000; //  chose port from here like 8080, 3001
+
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(3000,()=>{
+    console.log("JSON server is runnning")
+});
